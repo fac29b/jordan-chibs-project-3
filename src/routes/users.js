2b943bc8a3bcfdebedb492df986e9a88c54d40b2
Jordan <jlprogrammer_98@hotmail.com>
2024-01-16 19:02:30 +0000

    sending html file to route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
+const path = require('path');
 
-router.get('/', (req, res) => {
-    res.send('hello')
-})
+router.get("/", (req, res) => {
+  res.sendFile(path.resolve('public/index.html'), {data: [{name: "andrew"}]});
+});
 
 module.exports = router;
